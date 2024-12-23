cd6301a5800d94c1ff7444919d28770a38999199
SCCSMARTCODE <sccsmart247@gmail.com>
2024-12-23 18:11:55 +0100

    Basic Calculator UI with Glade

--- CalculatorApp/src/callbacks.h
+++ CalculatorApp/src/callbacks.h
@@ -1,3 +1,7 @@
 #pragma once
 
 #define UNUSED(x) (void)x
+
+#include <gtk/gtk.h>
+
+#define GLADE_SERIALIZED_FILE_PATH "ui/calculator.glade"
--- CalculatorApp/src/main.c
+++ CalculatorApp/src/main.c
@@ -1,8 +1,30 @@
 #include "callbacks.h"
 
 int main(int argc, char *argv[]) {  
-    UNUSED(argc);
-    UNUSED(argv);
+
+    GtkBuilder *builder;
+    GtkWidget *window;
+
+    gtk_init(&argc, &argv);
+
+    builder = gtk_builder_new_from_file(GLADE_SERIALIZED_FILE_PATH);
+    if (!builder){
+        g_print("Error Loading .glade file\n");
+        return 1;
+    }
+
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "main_window"));
+    if(!window){
+        g_print("Error Loading main_window from glade file\n");
+        return 1;
+    }
+
+    gtk_builder_connect_signals(builder, NULL);
+    g_signal_connect(window, "destroy", gtk_main_quit, NULL);
+
+    gtk_widget_show_all(window);
+
+    gtk_main();
 
 
     return 0;  
